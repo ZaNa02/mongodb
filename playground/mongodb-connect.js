cbcc5f1428e80a6faf53706bbcbe52e84345a4ae
Ania F <38281107+ZaNa02@users.noreply.github.com>
2018-08-20 14:34:09 +0200

    added client data

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,1 +1,39 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+  if (err) {
+    return console.log('unable to connect to mongo db server');
+
+  }
+  console.log('connected to mongo db server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed:false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // //insert new doc int Users(name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Ania',
+  //   age: 30,
+  //   location: 'HZO'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
+
+  client.close();
+});
